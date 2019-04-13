3505b87ca2aedb48b7008a113e3a61e301b1228e
Arunwebber <arunwebber@gmail.com>
2019-04-13 13:51:00 +0530

    Adding https over http

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ if(isset($_GET['logout'])){
 <?php
 if(isset($_SESSION['username'])){
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="https://www.w3.org/1999/xhtml">
 <head>
 <title>Chat - Eduladder</title>
 <h2>Welcome to eduladder open chat</h2>
@@ -47,7 +47,7 @@ if(isset($_SESSION['username'])){
         <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
     </form>
 </div>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
 <script type="text/javascript">
 // jQuery Document
 $(document).ready(function(){
