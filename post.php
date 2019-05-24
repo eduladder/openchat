9527d21034e06ed9f67e1f7f3f591bf1eb547537
VvePrime <vive021172@gmail.com>
2019-05-24 20:19:30 +0530

    date bug fixed

--- post.php
+++ post.php
@@ -3,7 +3,8 @@ session_start();
 if(isset($_SESSION['username'])){
     echo $text = $_POST['text'];
     $fp = fopen("log.html", 'a');
-    fwrite($fp, "<a target='_blank' href='../viewuser/$_SESSION[user_id]/$_SESSION[username]'><div class='msgln'>(".date("g:i A").") <b>".$_SESSION['username']."</b>:</a>".stripslashes(htmlspecialchars($text))."<br></div>");
+    date_default_timezone_set('Asia/Calcutta');
+    fwrite($fp, date("h:i A")."<a target='_blank' href='../viewuser/$_SESSION[user_id]/$_SESSION[username]'><div class='msgln'> <b>".$_SESSION['username']."</b>:</a>".stripslashes(htmlspecialchars($text))."<br></div>");
     fclose($fp);
 }
 ?>
